0870f8cc4d730f88c2831ba2520c816eb12204d0
Iosif <12469gr@saeinstitute.edu>
2020-01-10 21:19:08 +0200

    Script that implement map

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,37 @@
 //Autocomplete
 $( function() {
 
-  var classes = [
+  var entries = [];
+$.getJSON("entries.json", function(data) {
+    $.each(data, function(key, value) {
+        if ($.inArray(value.name, entries) === -1) {
+            entries.push(value.name)
+        }
+    })
+});
+
+  /* $( ".open" ).click(function initMap() {
+    $(".popup, .popup-content").addClass("active");
+    // The location of Greece
+    var greece = {lat: 39.0742, lng: -21.8243};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: greece});
+    // The marker, positioned at Greece
+    var marker = new google.maps.Marker({position: greece, map: map});
+  }); */
+  
+ $(".open").on("click", function(){
+    $(".popup, .popup-content").addClass("active");
+    });
+    
+  $(".close, .popup").on("click", function(){
+      $(".popup, .popup-content").removeClass("active");
+      });
+
+  //****Local Entries******//
+  
+  /* var classes = [
   "Athens",
   "Athens 1",
   "Athens 3",
@@ -9,12 +39,11 @@ $( function() {
   "Nea Smirni",
   "Palaio Faliro",
   "Palaio Psichiko"
-  ];
+  ]; */
 
   $( "#search" ).autocomplete({
     source: function(req, response) {
-      var results = $.ui.autocomplete.filter(classes, req.term);
-  
+      var results = $.ui.autocomplete.filter(entries, req.term);
       response(results.slice(0, 20));//for getting 20 results
      },
     minLength:2,
@@ -22,4 +51,6 @@ $( function() {
     delay:120,
     remoteDataType: 'json'
   });
+
+  
 } );
