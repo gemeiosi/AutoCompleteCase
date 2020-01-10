7ab07d323dd236b072d471a66cabe56f0adac1df
Iosif <12469gr@saeinstitute.edu>
2020-01-10 10:21:29 +0200

    Get first 20 results

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,23 @@ $( function() {
 
   var classes = [
   "Athens",
+  "Athens 1",
+  "Athens 3",
+  "Athens 4",
   "Nea Smirni",
   "Palaio Faliro",
-  "Psichiko"
+  "Palaio Psichiko"
   ];
 
   $( "#search" ).autocomplete({
-    source: classes,
+    source: function(req, response) {
+      var results = $.ui.autocomplete.filter(classes, req.term);
+  
+      response(results.slice(0, 20));//for getting 20 results
+     },
     minLength:2,
+    autoFocus:true,
+    delay:120,
+    remoteDataType: 'json'
   });
 } );
