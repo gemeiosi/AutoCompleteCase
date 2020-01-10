a2e07ff0eaaa2027353bf8634cba22ed790c795b
Iosif <12469gr@saeinstitute.edu>
2020-01-10 09:36:26 +0200

    Autocomplete script

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,15 @@
-// Menu Toggle Script
-  $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-  });
+//Autocomplete
+$( function() {
 
-//
+  var classes = [
+  "Athens",
+  "Nea Smirni",
+  "Palaio Faliro",
+  "Psichiko"
+  ];
+
+  $( "#search" ).autocomplete({
+    source: classes,
+    minLength:2,
+  });
+} );
